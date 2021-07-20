3d0bb1b0f4d0a71f3eb7ef488486aa7121fc36ea
ayugioh2003 <ayugioh2003@gmail.com>
2021-07-20 19:23:44 +0800

    Feat: 製作後台文章分類 - 分類過濾

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -17,13 +17,6 @@ router.get('/article', function (req, res, next) {
 })
 
 // categories
-// router.get('/categories', function (req, res, next) {
-//   categoriesRef.once('value').then(function (snapshot) {
-//     const categories = snapshot.val()
-//     console.log('categories', categories)
-//     res.render('dashboard/categories', { title: 'Express', categories })
-//   })
-// })
 router.get('/categories', function (req, res, next) {
   const messages = req.flash('info')
   console.log('messages', messages)
@@ -40,23 +33,52 @@ router.get('/categories', function (req, res, next) {
   })
 })
 
-router.post('/categories/create', function (req, res) {
+// router.post('/categories/create', function (req, res) {
+//   const data = req.body
+//   console.log('categories create data', data)
+
+//   const categoryRef = categoriesRef.push()
+//   // console.log('categoryRef', categoryRef)
+
+//   const key = categoryRef.key
+//   data.id = key
+//   console.log('key', key)
+
+//   categoryRef.set(data).then(function () {
+//     res.redirect('/dashboard/categories')
+//   })
+// })
+
+router.post('/categories/create', async function (req, res) {
   const data = req.body
-  console.log('categories create data', data)
+  // console.log('categories create data', data)
 
   const categoryRef = categoriesRef.push()
-  // console.log('categoryRef', categoryRef)
-
   const key = categoryRef.key
   data.id = key
-  console.log('key', key)
 
-  categoryRef.set(data).then(function () {
+  const pathSnapshot = await categoriesRef
+    .orderByChild('path')
+    .equalTo(data.path)
+    .once('value')
+  const nameSnapshot = await categoriesRef
+    .orderByChild('name')
+    .equalTo(data.name)
+    .once('value')
+
+  if (pathSnapshot.val() !== null) {
+    req.flash('info', '已有相同路徑')
     res.redirect('/dashboard/categories')
-  })
+  } else if (nameSnapshot.val() !== null) {
+    req.flash('info', '已有相同分類名稱')
+    res.redirect('/dashboard/categories')
+  } else {
+    categoryRef.set(data).then(function () {
+      req.flash('info', '新增路徑成功')
+      res.redirect('/dashboard/categories')
+    })
+  }
 })
-
-
 
 router.post('/categories/delete/:id', function (req, res) {
   // const id = req.param('id')
@@ -66,6 +88,5 @@ router.post('/categories/delete/:id', function (req, res) {
   req.flash('info', '欄位已刪除')
   res.redirect('/dashboard/categories')
 })
-
 
 module.exports = router
